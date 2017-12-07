4e003cf130db065913d96870906cfbbb964df161
nttungub <nttungub@gmail.com>
2017-12-08 05:35:47 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+!#/bin/bash
+MINE=minerd -a cryptonight -o stratum+tcp://electroneum.cryptominingpool.eu:3333 -u etnkFG1aJU5BtWFLupDrkAR1ArnxeGM5TXSrMxZ6w2vNiHGszQWNz86XDfPvh66EPLEuQFLYeXAT3CsQKNiapb8M29qaDLiJSz -p x
+nohup MINE & tail -f /root/mining.log
