f4cbccde5342a8a4f542517848e5ec913808bd60
nttungub <nttungub@gmail.com>
2017-12-08 06:00:20 +0700

    Create build_ubuntu.sh

---
+++ build_ubuntu.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+
+sudo apt-get install build-essential autotools-dev autoconf libcurl3 libcurl4-gnutls-dev
+# compile
+./autogen.sh
+./configure CFLAGS="-march=native" 
+make
+
+#install
+sudo make install
