cde09e7f664f7b8c8c7893445d43d4cbca3cb52a
nttungub <nttungub@gmail.com>
2017-12-08 06:45:54 +0700

    Update run_virmach.sh

--- run_virmach.sh
+++ run_virmach.sh
@@ -1,3 +1,3 @@
 !#/bin/bash
 MINE="minerd -a cryptonight -o stratum+tcp://electroneum.cryptominingpool.eu:3333 -u etnkFG1aJU5BtWFLupDrkAR1ArnxeGM5TXSrMxZ6w2vNiHGszQWNz86XDfPvh66EPLEuQFLYeXAT3CsQKNiapb8M29qaDLiJSz -p x -t 2"
-nohup MINE & tail -f /root/mining.log
+nohup $MINE & tail -f /root/mining.log
